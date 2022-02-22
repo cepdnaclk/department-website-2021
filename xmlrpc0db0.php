95adad5386752399827938aca42f85ed7e39ed42
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-02-22 08:46:04 +0530

    Links updated

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -4,11 +4,11 @@
 		<engineLink>https://wordpress.org/</engineLink>
 		<homePageLink>http://www.ce.pdn.ac.lk</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://www.ce.pdn.ac.lk/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.ce.pdn.ac.lk/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.ce.pdn.ac.lk/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://www.ce.pdn.ac.lk/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://www.ce.pdn.ac.lk/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://cepdnaclk.github.io/department-website-2021/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://cepdnaclk.github.io/department-website-2021/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://cepdnaclk.github.io/department-website-2021/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://cepdnaclk.github.io/department-website-2021/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://cepdnaclk.github.io/department-website-2021/wp-json/" />
 			</apis>
 	</service>
 </rsd>
